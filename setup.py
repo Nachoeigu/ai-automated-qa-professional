42996de93e346f45257dbfc2e653b9ac75aab142
Nacho Eigu 🇦🇷 <ignacio.eiguren@gmail.com>
2024-08-12 19:09:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='ai-automated-qa-professional',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     install_requires=[
       'langchain_openai',
       'python-dotenv',
@@ -11,7 +12,7 @@ setup(
       'langsmith'
     ],
     author='Nachoeigu',
-    author_email='ignacio.eiguren@gmail..com',
+    author_email='ignacio.eiguren@gmail.com',
     description='This software enables you to respond to job-related questions using your personal and professional information. It can be seamlessly integrated with external applications, such as bots, to automate responses in job application forms.',
     url='https://github.com/Nachoeigu/ai-automated-qa-professional',
     classifiers=[
